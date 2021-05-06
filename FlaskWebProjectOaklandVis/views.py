d1e77b2717e5a6ec4fb2b4f869ae3a1931ed9bde
Izaac Garcilazo <izgarcil@microsoft.com>
2021-05-06 09:21:23 -0700

    First commit

---
+++ FlaskWebProjectOaklandVis/views.py
@@ -0,0 +1,64 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from FlaskWebProjectOaklandVis import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/data')
+def data():
+    """Renders the home page."""
+    return render_template(
+        'data.html',
+        title='Data',
+        year=datetime.now().year,
+    )
+
+@app.route('/graphs')
+def graphs():
+    """Renders the home page."""
+    return render_template(
+        'graphs.html',
+        title='Graphs',
+        year=datetime.now().year,
+    )
+
+@app.route('/profile')
+def profile():
+    """Renders the profile page."""
+    return render_template(
+        'profile.html',
+        title='Profile',
+        year=datetime.now().year,
+        message='Your profile page.'
+    )
+
+@app.route('/messages')
+def messages():
+    """Renders the messages page."""
+    return render_template(
+        'messages.html',
+        title='Messages',
+        year=datetime.now().year,
+        message='Your messages page.'
+    )
+@app.route('/alert')
+def alert():
+    """Renders the profile page."""
+    return render_template(
+        'alert.html',
+        title='Alert',
+        year=datetime.now().year,
+        message='Your alerts page.'
+    )
